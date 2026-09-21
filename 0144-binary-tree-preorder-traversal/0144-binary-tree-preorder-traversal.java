ec2c9b2bd902cd338e7b65caa74c0f095a5c1d29
aditiT09 <adititiwari095@gmail.com>
2026-09-21 23:18:54 +0530

    Time: 0 ms (100%), Space: 42.8 MB (96.31%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,15 +14,31 @@
  * }
  */
 class Solution {
-    public List<Integer> ans= new LinkedList<>();
-    List<Integer> preorderTraversal(TreeNode root) {
-        if (root==null)
-        {
-            return ans;
-        }
-    ans.add(root.val);
-    preorderTraversal(root.left);
-    preorderTraversal(root.right);
-    return ans;      
+//     public List<Integer> ans= new LinkedList<>();
+//     List<Integer> preorderTraversal(TreeNode root) {
+//         if (root==null)
+//         {
+//             return ans;
+//         }
+//     ans.add(root.val);
+//     preorderTraversal(root.left);
+//     preorderTraversal(root.right);
+//     return ans;      
+//     }
+public void preorder(TreeNode root, List<Integer> ans)
+{
+    if(root==null)
+    {
+        return;
     }
+    ans.add(root.val);
+    preorder(root.left,ans);
+    preorder(root.right,ans);
 }
+List<Integer> preorderTraversal(TreeNode root)
+{
+    List<Integer> ans= new LinkedList<>();
+    preorder(root,ans);
+    return ans;
+}
+};
